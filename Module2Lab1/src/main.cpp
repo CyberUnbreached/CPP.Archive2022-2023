80feaf48f64f4d447672ce5e2e36c8527bb63fb9
Ian R <ian.n.rohan@gmail.com>
2024-08-28 11:11:19 -0500

    Add files via upload

---
+++ Module2Lab1/src/main.cpp
@@ -0,0 +1,19 @@
+//
+//main.cpp
+//Week2Lab1
+//Created by Ian Rohan on 8/30/2022
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	 // Variables
+	    string myName = "Ian Nicholas Rohan";
+	    string myEmail = "irohan2.student.alamo.edu";
+
+	 // Sentence
+	    cout << "My Name is " << myName << ", and my email is " << myEmail << endl;
+
+	    return 0;
+}
